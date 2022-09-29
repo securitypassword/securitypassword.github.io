d617e58385f1fdb6b7a9f5c64ba6d429aef13ac4
martin-sainos-demian <martin.sainos.demian@gmail.com>
2022-09-29 17:48:00 -0500

    jquerry

--- src/comps/Login.jsx
+++ src/comps/Login.jsx
@@ -4,6 +4,7 @@ export default class Login extends Component {
   render() {
     return (
       <div className="App">
+        
         <input type="text" id="user" />
         <br></br>
         <input type="button" id="send" value="actions" />
