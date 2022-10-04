348bc16df2a0f6bcac8b1886824e2d125caacc67
martin-sainos-demian <martin.sainos.demian@gmail.com>
2022-10-04 00:10:23 -0500

    backup

--- src/App.js
+++ src/App.js
@@ -1,17 +1,10 @@
 import logo from './logo.svg';
 import './App.css';
+import React from 'react';
 import Login from './comps/Login.jsx';
 
 
 function App() {
-  const [data, setData] = React.useState(null);
-
-  React.useEffect(() => {
-    fetch("https://glitch.com/~perfect-cream-wound")
-      .then((res) => res.json())
-      .then((data) => console.log(data.message));
-  }, []);
-
   return (
     <div className="App">
       security password
--- src/comps/Login.jsx
+++ src/comps/Login.jsx
@@ -8,12 +8,21 @@ export default class Login extends Component {
         
         <input type="text" id="user" />
         <br></br>
-        <input type="button" id="send" value="actions" />
+        <input type="button" id="send" value="action" />
       </div>
     )
   }
 }
 
 function codes(){
-  console.log("uwu")
+  fetch("https://glitch.com/~perfect-cream-wound")
+  .then((response) => response.json())  
+	.then((message) => console.log(message));
+  console.log("owo")
+  fetch("https://glitch.com/~perfect-cream-wound")
+  .then((response) => console.log(response));
+  console.log("ewe")
+  fetch("https://perfect-cream-wound.glitch.me/")
+  .then((response) => console.log(response));
+  console.log("iwi");
 }
