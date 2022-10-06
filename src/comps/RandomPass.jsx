2fae6f74cf5303e8602a2a62c488fa739a063a3e
martin-sainos-demian <martin.sainos.demian@gmail.com>
2022-10-06 18:48:51 -0500

    switch component

--- src/comps/RandomPass.jsx
+++ src/comps/RandomPass.jsx
@@ -1,18 +1,20 @@
 import React, { Component, startTransition } from 'react'
 import {Button} from 'react-native'
+import Switch from './Switch';
 import $ from 'jquery'
 import {en,de} from "./sec"
 
 export default class RandomPass extends Component {
-    render() {
-      return (
-        <div className="RandomPass">
-          <input type="text" id="pass" placeholder="password"></input>
-          <Button title="generate" onPress={gen}></Button>
-        </div>
-      )
-    }
+  render() {
+    return (
+      <div className="RandomPass">
+        <Switch title="a"></Switch>
+        <input type="text" id="pass" placeholder="password"></input>
+        <Button title="generate" onPress={gen}></Button>
+      </div>
+    )
   }
+}
   function gen(){
 
   }
--- src/comps/Switch.jsx
+++ src/comps/Switch.jsx
@@ -1,8 +1,10 @@
 import React, { Component, startTransition } from 'react'
 import {Button} from 'react-native'
-
 export default class Switch extends Component {
     render() {
-      return ()
+      return (
+      <Button title={this.title} onPress={this.pressed=!this.pressed} id={this.id+"button"}></Button>
+      )
     }
 }
+var pressed=false;
