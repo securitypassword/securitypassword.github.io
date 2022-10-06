c12a6c88d69c1e04351c2da49b62356f7c53e73b
martin-sainos-demian <martin.sainos.demian@gmail.com>
2022-10-06 17:44:41 -0500

    url friendly fix

--- src/comps/sec.js
+++ src/comps/sec.js
@@ -3,8 +3,8 @@ var CryptoJS = require("crypto-js");
 var key="i forgor :skull:"
 
 export function de(text){
-    return CryptoJS.DES.decrypt(text, key).replace(/\+/g, '-').replace(/\//g, '_').toString(CryptoJS.enc.Utf8)
+    return CryptoJS.DES.decrypt(text.replace(/-/g, '+').replace(/_/g, '/'), key).toString(CryptoJS.enc.Utf8)
   }
   export function en(text){
-    return CryptoJS.DES.encrypt(text, key).toString().replace(/-/g, '+').replace(/_/g, '/')
+    return CryptoJS.DES.encrypt(text, key).toString().replace(/\+/g, '-').replace(/\//g, '_')
   }
