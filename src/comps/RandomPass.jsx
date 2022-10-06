6ab455df05239dda4f3f0a602a33d82350d34947
martin-sainos-demian <martin.sainos.demian@gmail.com>
2022-10-06 18:33:28 -0500

    gen switches

--- src/comps/RandomPass.jsx
+++ src/comps/RandomPass.jsx
@@ -1,17 +1,18 @@
 import React, { Component, startTransition } from 'react'
+import {Button} from 'react-native'
 import $ from 'jquery'
 import {en,de} from "./sec"
 
 export default class RandomPass extends Component {
-
-
-
     render() {
       return (
         <div className="RandomPass">
           <input type="text" id="pass" placeholder="password"></input>
-          <input type="button" value="generate"></input>
+          <Button title="generate" onPress={gen}></Button>
         </div>
       )
     }
+  }
+  function gen(){
+
   }
---
+++ src/comps/Switch.jsx
@@ -0,0 +1,8 @@
+import React, { Component, startTransition } from 'react'
+import {Button} from 'react-native'
+
+export default class Switch extends Component {
+    render() {
+      return ()
+    }
+}
