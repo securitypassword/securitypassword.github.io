e797ca1ac4e657996f569a449ddd9c696cfde11d
martin-sainos-demian <martin.sainos.demian@gmail.com>
2022-10-13 16:31:05 -0500

    update front by me

--- src/App.js
+++ src/App.js
@@ -2,23 +2,16 @@ import logo from './logo.svg';
 import './App.css';
 import React from 'react';
 import Login from './comps/Login';
+import NavPrincipal from './comps/NavPrincipal';
 import TitlePass from './comps/TitlePass';
-import NewPass from './comps/NewPass';
-import GetPass from './comps/GetPass';
-import Encoder from './comps/Encoder';
-import Register from './comps/Register';
 
 
 function App() {
   return (
     <div className="App">
       <script type="text/javascript" src="https://cdnjs.cloudflare.com/ajax/libs/crypto-js/3.1.2/rollups/aes.js"></script>
-      <TitlePass></TitlePass>
-      <Login id="login"></Login>
-      <NewPass></NewPass>
-      <GetPass></GetPass>
-      <Encoder></Encoder>
-      <Register></Register>
+      <TitlePass id="titlePass"></TitlePass>
+      <NavPrincipal></NavPrincipal>
         <p id="logId" hidden></p>
     </div>
   );
