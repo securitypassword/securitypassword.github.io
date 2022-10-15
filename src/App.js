a2b803ef47cfe2317fca36d105e9986aaf6bc6d4
martin-sainos-demian <martin.sainos.demian@gmail.com>
2022-10-15 15:32:28 -0500

    pass

--- src/App.js
+++ src/App.js
@@ -7,20 +7,26 @@ import Register from './comps/Register';
 import Navigation from './comps/Navigation';
 import $ from 'jquery'
 import {
-  BrowserRouter as Router,
-  Link
+  BrowserRouter,
+  Routes,
+  Route,
+  Link,
+  HashRouter,
 } from "react-router-dom";
-import { Routes ,Route } from 'react-router-dom';
 
 function App() {
   return (
     <div className="App"><script type="text/javascript" src="https://cdnjs.cloudflare.com/ajax/libs/crypto-js/3.1.2/rollups/aes.js"></script>
-    <Routes>
-    <Route path='/' element={<Navigation></Navigation>} />
-    <Route path='/login' element={<Login/>} />
-    <Route path='/register' element={<Register/>} />
-    <Route path='/pass/:usu_id' element={<Pass  /> } />
-    </Routes>
+    
+    <HashRouter basename="/">
+    <Link to="/login">Home</Link>
+      <Routes>
+        <Route path='/' element={<Navigation></Navigation>} />
+        <Route path='/login' element={<Login/>} />
+        <Route path='/register' element={<Register/>} />
+        <Route path='/pass/:usu_id' element={<Pass  /> } />
+      </Routes>
+    </HashRouter>
     <p id="logId" hidden></p>
     </div>
   );
