dcfb64075ceade49d7e9b65d20cdb724e23d8158
Zuka <111401951+Zuka505@users.noreply.github.com>
2022-11-29 12:22:15 -0600

    Add files via upload

--- src/comps/User/Logout.js
+++ src/comps/User/Logout.js
@@ -2,7 +2,7 @@ import React from 'react';
 
 const Logout = () => {
     window.sessionStorage.setItem("token", "")
-    window.location.href = "/"
+    window.location.href = "/login"
     return(<></>)
 }
 
