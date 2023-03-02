ae6601faff7a20534a3f2033358d339433aed2dd
martin-sainos-demian <martin.sainos.demian@gmail.com>
2023-03-02 14:48:28 -0600

    a

--- src/comps/Password/NavbarPass.js
+++ src/comps/Password/NavbarPass.js
@@ -1,39 +1,51 @@
 import React from 'react';
 import { Outlet, Link } from "react-router-dom";
+import "../Navbar/Navbar.css"
 
 const NavbarPass = () => {
     return(
         <>
         <br></br>
         <nav>
-            <Link
-            to='/newpassword'
-            className='nav-links'>
-            New Password
-            </Link>
-            <Link
-            to='/passwords'
-            className='nav-links'>
-            Get Passwords
-            </Link>
-            <Link
-            to='/bin'
-            className='nav-links'>
-            Bin
-            </Link>
-            <Link
-            to='/settings'
-            className='nav-links'>
-            Settings
-            </Link>
-            <Link
-            to='/logout'
-            className='nav-links'>
-            LogOut
-            </Link>
-
+            <div className='navbar-container'>
+                <div className='nav-item'>
+                    <Link
+                    to='/newpassword'
+                    className='nav-links'>
+                    New Password
+                    </Link>
+                </div>
+                <div className='nav-item'>
+                    <Link
+                    to='/passwords'
+                    className='nav-links'>
+                    Get Passwords
+                    </Link>
+                </div>
+                <div className='nav-item'>
+                    <Link
+                    to='/bin'
+                    className='nav-links'>
+                    Bin
+                    </Link>
+                </div>
+                <div className='nav-item'>
+                    <Link
+                    to='/settings'
+                    className='nav-links'>
+                    Settings
+                    </Link>
+                </div>
+                <div className='nav-item'>
+                    <Link
+                    to='/logout'
+                    className='nav-links'>
+                    LogOut
+                    </Link>
+                </div>
+            </div>
         </nav>
-        <Outlet />
+        <Outlet></Outlet>
         </>
     )
 }
