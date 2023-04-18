78c2f0a2ea86c618e2dfe495016a6e9656d25e33
martin-sainos-demian <martin.sainos.demian@gmail.com>
2023-04-17 22:36:51 -0600

    count

--- src/comps/Admin/Admin.js
+++ src/comps/Admin/Admin.js
@@ -1,11 +1,13 @@
 import React from "react";
 import NavbarAdmin from "./NavbarAdmin";
+import Count from "./Count";
 
 //la interfaz para admins
 const Admin = () => {
     return(
         <>
         <NavbarAdmin></NavbarAdmin>
+        <Count></Count>
         </>
     )
 }
---
+++ src/comps/Admin/Count.js
@@ -0,0 +1,40 @@
+import React from "react";
+import {  useState, useEffect } from 'react';
+import axios from "../../api/axios";
+
+//const COUNT_URL = process.env.API_URL + "/count"
+const COUNT_URL = "https://securitypassword.cyclic.app/count"
+
+export const getCount = async () => {
+    let token = window.sessionStorage.getItem("token")
+    const query = {
+        token:token
+    }
+    const resp = await axios.post(COUNT_URL,
+        JSON.stringify(query),
+        {
+            headers: { 'Content-Type': 'application/json' },
+            withCredentials: true
+        }
+    );
+    return resp.data
+}
+
+const Count = () => {
+    const [counts, setCounts] = useState("")
+
+    useEffect(() => {
+        const resp = getCount().then(function(resp){
+            setCounts(resp)
+            return resp
+        })
+    }
+    ,[])
+    return(
+        <>
+        {counts}
+        </>
+    )
+}
+
+export default Count
