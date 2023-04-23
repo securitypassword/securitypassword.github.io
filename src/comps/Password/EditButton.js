bf748e9e5bef79230dcd6352202e2381df082c5f
martin-sainos-demian <martin.sainos.demian@gmail.com>
2023-04-22 18:25:05 -0600

    action

--- src/comps/Password/EditButton.js
+++ src/comps/Password/EditButton.js
@@ -4,13 +4,13 @@ import { useNavigate } from 'react-router-dom';
 const EditButton = (parms) => {
     const navigate = useNavigate();
     const EDIT_URL = "/editpassword/"+parms.id
-    const act = () => {
+    const action = () => {
         navigate(EDIT_URL);
     }
     //redireccion para modo de edicion de contraseña
     return(
         <>
-        <button onClick={act}>Editar</button>
+        <button onClick={action}>Editar</button>
         </>
     )
 }
