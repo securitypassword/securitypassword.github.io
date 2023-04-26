bd2a2c60176ad805c6894148d389c9f9d76ea912
Antonio-Zabdiel <cabrera.aguirre.antonio.zabdiel@gmail.com>
2023-04-26 16:01:29 -0600

    .

--- src/comps/Legal.js
+++ src/comps/Legal.js
@@ -2,10 +2,30 @@
 import Navbar from "./Navbar/Navbar";
 
 const Legal = () => {
+    const styles = {
+        main: {
+            marginLeft: "100px",
+          },
+      
+      };
+
     return(
         <>
         <Navbar />
-        aviso legal aviso legal
+            <div>
+                <center>
+                    Terminos de uso y condiciones
+                </center>
+            </div>
+            <div className="main">
+                <h3>1.-Quienes somos</h3>
+                <br/>
+                Prestamos este servicio para facilitar la gestión de las contraseñas, mediante el almacenamiento de estas en la nube, este software es de uso gratuito para todos los usuarios.
+                <br/><br/>
+                <h3>2.- Cómo ponerte en contacto con nosotros</h3>
+                Escribe un correo a : sec.tech.contact@gmail.com
+
+            </div>
         </>
     )
 }
