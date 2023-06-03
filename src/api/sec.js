4d1d3d9361c205bb191af591dfa32080b3ed2e7b
Antonio-Zabdiel <cabrera.aguirre.antonio.zabdiel@gmail.com>
2023-06-03 09:42:24 -0600

    a

--- src/api/sec.js
+++ src/api/sec.js
@@ -1,4 +1,4 @@
 export const from64 = function(text){
-  return decodeURIComponent(escape(atob(text.replace(/-/g, "+")
-  .replace(/_/g, "/"))))
-}
+    return unescape(atob(text.replace(/\-/g, "+")
+    .replace(/\_/g, "/")))
+  }
